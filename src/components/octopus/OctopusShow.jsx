313584d8a00d1b1192cc162bed30a41ce5da0a3b
joefreeman8 <joefreeman8@gmail.com>
2024-07-05 18:05:17 +0100

    refactored octopus sightings into it's own component

--- src/components/octopus/OctopusShow.jsx
+++ src/components/octopus/OctopusShow.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import SightingShow from "../sightings/SightingShow"
 
 
 export default function OctopusShow() {
@@ -24,10 +25,7 @@ export default function OctopusShow() {
 
   console.log(octopusData)
 
-  function formatDate(dateStr) {
-    const [year, month, day] = dateStr.split('-')
-    return `${day}-${month}-${year}`
-  }
+
 
   return (
     <>
@@ -41,37 +39,13 @@ export default function OctopusShow() {
                     {octopusData.name} <span className="text-sm">({octopusData.scientific_name})</span>
                   </h1>
                   <p><strong className="text-white">Bio:</strong> {octopusData.description}</p>
-                  <p><strong className="text-white">Maximum life span:</strong> {octopusData.life_span} years.</p>
+                  <p><strong className="text-white">Life span:</strong> {octopusData.life_span} years.</p>
                 </div>
               </div>
             </div>
             <div className="card px-4 md:w-1/2">
               <div className="card card-body space-y-4 flex justify-center items-center md:w-full" >
-                <h1 className="text-white text-lg font-bold">Recent Sightings</h1>
-                <div className="flex justify-center">
-                  <table className="table table-zebra">
-                    <thead className="text-sm">
-                      <tr>
-                        <th className="text-white">Date</th>
-                        <th className="text-white">Location</th>
-                        <th className="text-white">User</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {octopusData.sightings.map((sighting, idx) => (
-                        <tr key={sighting.id}>
-                          {idx < 10 &&
-                            <>
-                              <td>{formatDate(sighting.date)}</td>
-                              <td>{sighting.location}</td>
-                              <td>{sighting.sighting_owner.username}</td>
-                            </>
-                          }
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
+                <SightingShow sightings={octopusData.sightings} />
               </div>
             </div>
           </div>
--- src/components/sightings/SightingShow.jsx
+++ src/components/sightings/SightingShow.jsx
@@ -1,6 +1,39 @@
 
-export default function SightingShow() {
+export default function SightingShow({ sightings }) {
+
+
+  function formatDate(dateStr) {
+    const [year, month, day] = dateStr.split('-')
+    return `${day}-${month}-${year}`
+  }
+
   return (
-    <div>SightingShow</div>
+    <>
+      <h1 className="text-white text-lg font-bold">Recent Sightings</h1>
+      <div className="flex justify-center">
+        <table className="table table-zebra">
+          <thead className="text-sm">
+            <tr>
+              <th className="text-white">Date</th>
+              <th className="text-white">Location</th>
+              <th className="text-white">User</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sightings.map((sighting, idx) => (
+              <tr key={sighting.id}>
+                {idx < 10 &&
+                  <>
+                    <td>{formatDate(sighting.date)}</td>
+                    <td>{sighting.location}</td>
+                    <td>{sighting.sighting_owner.username}</td>
+                  </>
+                }
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
