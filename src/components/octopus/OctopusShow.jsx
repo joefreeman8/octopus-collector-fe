fe836ca39dbc35e979aa8aa01b0b8a2459afb4ac
joefreeman8 <joefreeman8@gmail.com>
2024-07-10 11:49:44 +0100

    added delete functionality so octopus can be deleted

--- src/components/octopus/OctopusShow.jsx
+++ src/components/octopus/OctopusShow.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import SightingShow from "../sightings/SightingShow"
 import { getSingleOctopus } from "../../lib/api"
 import AddSighting from "../sightings/AddSighting"
+import axios from "axios"
+import { toast } from "react-toastify"
 
 
 export default function OctopusShow() {
@@ -11,6 +13,7 @@ export default function OctopusShow() {
   const [sightingAdded, setSightingAdded] = useState(false)
 
   const { id } = useParams()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,39 +28,66 @@ export default function OctopusShow() {
     fetchData()
   }, [id, sightingAdded])
 
+  async function handleDelete() {
+    const token = localStorage.getItem('token')
+    try {
+      await axios.delete(`http://localhost:8000/api/octopus/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      navigate('/octopus')
+    } catch (err) {
+      const error = err.response.data.detail
+      toast.info(error, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      })
+    }
+  }
+
 
   return (
     <div className="h-screen">
       {octopusData && (
-        <div className="flex justify-center items-center mt-24">
-          <div className="text-center flex flex-col md:flex-row gap-4 justify-center md:items-start">
-            <div className="p-4 md:w-1/2">
-              <div className="card bg-base-100 bg-opacity-90 mb-5 shadow-xl">
-                <div className="card-body flex items-center">
-                  <h1 className="text-white card-title text-center w-fit">
-                    {octopusData.name}
-                    <span className="text-sm">
-                      ({octopusData.scientific_name})
-                    </span>
-                  </h1>
-                  <p><strong className="text-white">Bio:</strong> {octopusData.description}</p>
-                  <p><strong className="text-white">Life span:</strong> {octopusData.life_span} years.</p>
+        <>
+          <button onClick={handleDelete} className="btn btn-error">Delete</button>
+          <div className="flex justify-center items-center mt-24">
+            <div className="text-center flex flex-col md:flex-row gap-4 justify-center md:items-start">
+              <div className="p-4 md:w-1/2">
+                <div className="card bg-base-100 bg-opacity-90 mb-5 shadow-xl">
+                  <div className="card-body flex items-center">
+                    <h1 className="text-white card-title text-center w-fit">
+                      {octopusData.name}
+                      <span className="text-sm">
+                        ({octopusData.scientific_name})
+                      </span>
+                    </h1>
+                    <p><strong className="text-white">Bio:</strong> {octopusData.description}</p>
+                    <p><strong className="text-white">Life span:</strong> {octopusData.life_span} years.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="card px-4 md:w-1/2">
+                <div className="card card-body space-y-4 flex justify-center items-center md:w-full" >
+                  <SightingShow
+                    sightings={octopusData.sightings}
+                    sightingsThisWeek={octopusData.sightings_this_week}
+                    sightingsThisMonth={octopusData.sightings_this_month}
+                    octopusName={octopusData.name}
+                  />
+                  <AddSighting id={id} setSightingAdded={setSightingAdded} />
                 </div>
               </div>
             </div>
-            <div className="card px-4 md:w-1/2">
-              <div className="card card-body space-y-4 flex justify-center items-center md:w-full" >
-                <SightingShow
-                  sightings={octopusData.sightings}
-                  sightingsThisWeek={octopusData.sightings_this_week}
-                  sightingsThisMonth={octopusData.sightings_this_month}
-                  octopusName={octopusData.name}
-                />
-                <AddSighting id={id} setSightingAdded={setSightingAdded} />
-              </div>
-            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   )
