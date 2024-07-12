74cc2259983177f1feb8f3fa1ce2492096263818
joefreeman8 <joefreeman8@gmail.com>
2024-07-12 11:35:29 +0100

    pagination added for octopus images

--- src/components/images/ImagesShow.jsx
+++ src/components/images/ImagesShow.jsx
@@ -2,28 +2,52 @@ import { useState } from "react"
 
 export default function ImagesShow({ images }) {
 
+  // * CAROUSAL LOGIC
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
-  const goToPreviousImage = () => {
+  const goToPreviousImageCarousal = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1))
   }
 
-  const goToNextImage = () => {
+  const goToNextImageCarousal = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)
   }
+
+  // * PAGINATION LOGIC
+  const [currentPage, setCurrentPage] = useState(1)
+  const imagesPerPage = 4
+
+  const indexOfLastImage = currentPage * imagesPerPage
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage
+  const currentImage = images.slice(indexOfFirstImage, indexOfLastImage)
+
+  const totalPages = Math.ceil(images.length / imagesPerPage)
+
+  function nextPage() {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  function prevPage() {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
   return (
     <>
       <div className="mt-5 sm:grid sm:grid-cols-2 sm:gap-3">
         {/* Carousel for small screens */}
         <div className="flex sm:hidden ">
           {images && images.length > 0 && (
-            <div className="carousel-item relative w-full card card-compact bg-base-300 shadow-xl text-center">
+            <div className="carousel-item relative w-full card card-compact bg-base-100 bg-opacity-90 shadow-xl text-center">
               <figure>
                 <img src={images[currentImageIndex].document} alt={images[currentImageIndex].title} />
               </figure>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <button onClick={goToPreviousImage} className="btn btn-circle btn-xs">❮</button>
-                <button onClick={goToNextImage} className="btn btn-circle btn-xs">❯</button>
+                <button onClick={goToPreviousImageCarousal} className="btn btn-circle btn-xs">❮</button>
+                <button onClick={goToNextImageCarousal} className="btn btn-circle btn-xs">❯</button>
               </div>
               <div className="card-body flex justify-center items-center">
                 <p className="card-title">{images[currentImageIndex].title}</p>
@@ -34,21 +58,39 @@ export default function ImagesShow({ images }) {
       </div>
       {/* Grid for Larger Screens */}
       <div className="hidden sm:grid">
-        {images && (
-          <div className="mt-5 sm:grid sm:grid-cols-2 sm:gap-3">
-            {images.map((image) => (
-              <div key={image.id} className="card card-compact bg-base-300 shadow-xl w-full">
-                <figure className="">
-                  <img className="h-56 2xl:h-80 w-full" src={image.document} alt={image.title} />
-                </figure>
-                <div className="card-body text-center flex items-center">
-                  <p className="card-title justify-center text-sm">{image.title}</p>
-                  <p>By: {image.image_owner.username}</p>
+        <div className="flex justify-between items-center my-4">
+          <button
+            data-theme="nord"
+            className="btn btn-outline btn-circle btn-accent mx-2"
+            onClick={prevPage}
+            disabled={currentPage === 1}
+          >
+            «
+          </button>
+          {images && (
+            <div className="mt-5 sm:grid sm:grid-cols-2 sm:gap-3">
+              {currentImage.map((image) => (
+                <div key={image.id} className="card card-compact bg-base-100 bg-opacity-90 shadow-xl w-full">
+                  <figure className="">
+                    <img className="h-56 2xl:h-80 w-full" src={image.document} alt={image.title} />
+                  </figure>
+                  <div className="card-body text-center flex items-center">
+                    <p className="card-title justify-center text-sm">{image.title}</p>
+                    <p>By: {image.image_owner.username}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
+              ))}
+            </div>
+          )}
+          <button
+            data-theme="nord"
+            className="btn btn-outline btn-circle btn-accent mx-2"
+            onClick={nextPage}
+            disabled={currentPage === totalPages}
+          >
+            »
+          </button>
+        </div>
       </div>
     </>
   )
