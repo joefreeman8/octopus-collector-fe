aa3174b4d258eac0a4857dbcee0cc864bfd31b4a
joefreeman8 <joefreeman8@gmail.com>
2024-07-19 11:19:07 +0100

    added mobile responsiveness to homepage, going to push and test on my phone

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -1,21 +1,76 @@
-import OpenAIRequests from "../openAI/OpenAIRequests";
+import OpenAI from "openai"
+import { useState } from "react"
+
 
 export default function Home() {
 
+  const [openAIResponse, setopenAIResponse] = useState('Fun fact goes here....')
+  let isSubmitting = false
 
+  const OPENAI_API_KEY = import.meta.env.VITE_APP_OPENAI_API_KEY
+
+  const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
+  })
+
+  const octopusList = [
+    'Common Octopus',
+    'Giant Pacific Octopus',
+    'Blue-Ringed Octopus',
+    'Mimic Octopus',
+    'Dumbo Octopus',
+    'Vampire Squid',
+    'Coconut Octopus',
+    'Atlantic Pygmy Octopus',
+    'Striped Pyjama Octopus',
+    'Red Octopus',
+    'Maori Octopus',
+    'Atlantic White-spotted Octopus',
+    'Night Octopus',
+    'Mosaic Octopus'
+  ]
+
+
+
+  async function handleSubmit() {
+    if (isSubmitting) return // Prevent multiple submissions
+    isSubmitting = true
+    setopenAIResponse('Working on it... 🐙')
+    const randomOctopus = octopusList[Math.floor(Math.random() * octopusList.length)]
+
+    console.log(randomOctopus)
+
+    try {
+      const { choices } = await openai.chat.completions.create({
+        model: import.meta.env.VITE_APP_OPENAI_MODEL,
+        messages: [
+          { role: 'user', content: `Give me a really cool octopus fact for this octopus: ${randomOctopus}, try to find something unique about it. Failing that, provide a generic octopus fact. Ensure this text is 90 words or less.` }
+        ],
+        max_tokens: 200
+
+      })
+      setopenAIResponse(choices[0].message.content)
+
+    } catch (error) {
+      console.error('Error:', error)
+    } finally {
+      isSubmitting = false // Reset the flag after request
+    }
+  }
+
+  console.log(openAIResponse)
 
   return (
-    <div className="h-screen flex justify-center">
-      <div id="octocat"></div>
+    <div className="min-h-screen flex justify-center">
       <div className="container h-screen flex flex-col items-center">
-        <div className="mt-5 h-1/5 flex items-end justify-center">
-          <div className="flex flex-col">
-            <h1 className="text-white text-4xl font-bold">Welcome to the Octopus Collector</h1>
-            <p className="text-lg mt-5">Here is a hub where you can browse all the different Octopus, check out their photos & say when you spotted them!</p>
+        <div className="mt-10 md:mt-5 h-1/5 flex items-end justify-center">
+          <div className="flex flex-col p-5 md:p-0">
+            <h1 className="text-white text-3xl md:text-4xl font-bold">Welcome to the Octopus Collector</h1>
+            <p className="text-base md:text-lg mt-5">Here is a hub where you can browse all the different Octopus, check out their photos & say when you spotted them!</p>
           </div>
         </div>
-        <div className="container h-4/5 mt-12 flex flex-row justify-around">
-
+        <div className="flex flex-col px-5 md:px-0 md:container md:h-4/5 md:mt-12 md:flex-row md:justify-around">
           <div className="my-5 flex md:w-1/3 card card-bordered bg-base-100 bg-opacity-60">
             <p className="px-5 pt-5">
               Why on Earth would somebody spend hours of their time making a project such as this I hear you ask?
@@ -38,7 +93,14 @@ export default function Home() {
               Special shout out to squid and cuttlefish too.
             </p>
           </div>
-          <OpenAIRequests />
+          <div className="my-5 flex md:w-1/3 card card-bordered bg-base-100 bg-opacity-60">
+            <h2 className="text-center text-lg font-bold p-5">Octopus Facts</h2>
+            <span className="text-xs text-center font-bold italic">please note openAI occasionally likes to give repeated answers</span>
+            <button className='btn btn-accent my-5 mx-28' onClick={handleSubmit}>OctopusAI Button</button>
+            {openAIResponse && (
+              <p className="p-5">{openAIResponse}</p>
+            )}
+          </div>
         </div>
       </div>
     </div >
